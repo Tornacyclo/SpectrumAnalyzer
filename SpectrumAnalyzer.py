22a6b06fa33f16142be26aef788f9300c40e5b7e
Tornacyclo <65087331+Tornacyclo@users.noreply.github.com>
2022-12-27 03:09:34 +0100

    Create SpectrumAnalyzer.py

---
+++ SpectrumAnalyzer.py
@@ -0,0 +1,19 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# load the image into a NumPy array
+image = np.array(Image.open('image.jpg'))
+
+# compute the fast Fourier transform of the image
+fourier = np.fft.fft2(image)
+
+# shift the zero frequency component to the center of the spectrum
+fourier_shift = np.fft.fftshift(fourier)
+
+# take the magnitude of the transformed image
+magnitude = np.abs(fourier_shift)
+
+# plot the magnitude of the transformed image
+plt.imshow(magnitude, cmap='gray')
+plt.show()
